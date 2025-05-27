db01d6aebc9ba037773a9990d5134300ff65ae9c
hVie1314 <1212hoangviet@gmail.com>
2025-05-27 11:52:12 +0700

    Refactor MyCrypto class to improve key management and encryption methods; update methods to accept employee ID for key operations.

--- MyCrypto.py
+++ MyCrypto.py
@@ -1,5 +1,4 @@
 import os
-import base64
 import hashlib
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
@@ -7,66 +6,76 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 class MyCrypto:
 
     KEY_FOLDER = "key"
-    PRIVATE_KEY_PATH = os.path.join(KEY_FOLDER, "private_key.pem")
 
 
     def __init__(self):
-        if os.path.exists(self.PRIVATE_KEY_PATH):
-            # Load private key from file
-            with open(self.PRIVATE_KEY_PATH, "rb") as key_file:
-                self.private_key = serialization.load_pem_private_key(
+        # Create key folder to store key if it does not exist
+        os.makedirs(self.KEY_FOLDER, exist_ok=True)
+
+
+    def _get_key_path(self, id: str):
+        return os.path.join(self.KEY_FOLDER, f"{id}.pem")
+
+
+    def _load_or_create_keys(self, id: str):
+        key_path = self._get_key_path(id)
+
+        if os.path.exists(key_path):
+            with open(key_path, "rb") as key_file:
+                private_key = serialization.load_pem_private_key(
                     key_file.read(),
                     password=None,
                 )
-            # Get public key from private key
-            self.public_key = self.private_key.public_key()
-            print("Private key was loaded!")
-        else:
-            # Generate new RSA key pair and save private key to file 
-            self.private_key, self.public_key = self.generate_key()
-            self.save_key()
-            
+            public_key = private_key.public_key()
 
-    def generate_key(self):
-        private_key = rsa.generate_private_key(
+        else:
+            private_key = rsa.generate_private_key(
                 public_exponent=65537,
                 key_size=2048
             )
-        public_key = private_key.public_key()
-        return private_key, public_key
+            public_key = private_key.public_key()
+            private_pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+            with open(key_path, "wb") as key_file:
+                key_file.write(private_pem)
+                print(f"Generated and saved key for id {id}")
+
+        return public_key, private_key
     
 
-    def save_key(self):
-        os.makedirs(os.path.dirname(self.PRIVATE_KEY_PATH), exist_ok=True)
+    def get_public_key(self, id: str) -> str:
+        """
+        Get public key for given id
+        :param id: employee id
+        :return: public key in PEM format
+        """
 
-        private_pem = self.private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        )
-
-        with open(self.PRIVATE_KEY_PATH, "wb") as key_file:
-            key_file.write(private_pem)
-        print("Private key saved in ", self.PRIVATE_KEY_PATH)
-
-
-    def get_public_key(self):
-        public_pem = self.public_key.public_bytes(
+        public_key, _ = self._load_or_create_keys(id)
+        pem = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
-        lines = public_pem.decode().splitlines()
+
+        lines = pem.decode().splitlines()
         public_key_value = ''.join(line for line in lines if "BEGIN" not in line and "END" not in line)
+        
         return public_key_value
 
 
-    def encrypt_salary(self, salary: str) -> bytes:
+    def encrypt_salary(self, id: str, salary: str) -> bytes:
+
         """
-        Encrypt salary by using RSA and public key
+        Encrypt salary by using RSA and public key for given id
+        :param id: employee id
         :param salary: string of salary value
         :return: encrypted salary
         """
-        encrypted_salary = self.public_key.encrypt(
+
+        public_key, _ = self._load_or_create_keys(id)
+        encrypted_salary = public_key.encrypt(
             salary.encode(),
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA1()),
@@ -74,16 +83,24 @@ class MyCrypto:
                 label=None
             )
         )
-        return encrypted_salary
+
+        return "0x" + encrypted_salary.hex()
 
 
-    def decrypt_salary(self, ciphertext: bytes) -> str:
+    def decrypt_salary(self, id: str, ciphertext: bytes) -> str:
+
         """
-        Decrypt salary by using RSA and private key
+        Decrypt salary by using RSA and private key for given id
+        :param id: employee id
         :param ciphertext: encrypted salary 
-        :return: slary value
+        :return: salary value
         """
-        decrypted_salary = self.private_key.decrypt(
+
+        # Remove "0x" prefix
+        ciphertext = bytes.fromhex(ciphertext[2:])
+
+        _, private_key = self._load_or_create_keys(id)
+        decrypted_salary = private_key.decrypt(
             ciphertext,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA1()),
@@ -91,18 +108,20 @@ class MyCrypto:
                 label=None
             )
         )
+
         return decrypted_salary.decode()
 
 
     def hash_password_by_sha1(self, password: str) -> str:
+
         """
         Hash password by using SHA1 algorithm
         :param password: password string 
         :return: hash string in hex
         """
-        
+
         sha1_hash = hashlib.sha1(password.encode()).hexdigest()
-        return sha1_hash
+        return "0x" + sha1_hash
 
 
 
@@ -110,21 +129,25 @@ class MyCrypto:
 # USAGE
 # -------------------------------
 if __name__ == "__main__":
-    # Init MyCrypto Instance
+
     my_crypto = MyCrypto()
 
-    salary = "15000000"
+    # Enter data here
+    salary = "100000000"
     password = "abc123"
+    employee_id = "NV05"
 
-    public_key = my_crypto.get_public_key()
-    print(f"Public key:  {public_key} \n")
+    public_key = my_crypto.get_public_key(employee_id)
+    print(f"\nPublic key for {employee_id}:  {public_key}")
+    print("\n" + "-" * 80 + "\n")
+    encrypted_salary = my_crypto.encrypt_salary(employee_id, salary)
+    print(f"Encryp salary (hex): {encrypted_salary}")
+    print("\n" + "-" * 80 + "\n")
 
-    encrypted_salary = my_crypto.encrypt_salary(salary)
-    print(f"Encryp salary (base64):  {base64.b64encode(encrypted_salary).decode()}")
-    print(f"Encryp salary (hex):  {(encrypted_salary).hex().upper()}")
-
-    decrypted_salary = my_crypto.decrypt_salary(encrypted_salary)
-    print(f"Decryp salary:  {decrypted_salary}")
+    decrypted_salary = my_crypto.decrypt_salary(employee_id, encrypted_salary)
+    print(f"Decryp salary: {decrypted_salary}")
+    print("\n" + "-" * 80 + "\n")
 
     hashed_password = my_crypto.hash_password_by_sha1(password)
-    print(f"Hash password by SHA1:  {hashed_password}")
+    print(f"Hash password by SHA1: {hashed_password}")
+    print("\n" + "-" * 80 + "\n")
