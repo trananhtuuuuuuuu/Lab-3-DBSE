60c0be18215a970352fecb688f8a57574e354717
hVie1314 <1212hoangviet@gmail.com>
2025-05-27 11:52:31 +0700

    Update database connection settings and add SQL script for employee insertion

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import pyodbc
 def get_connection():
     return pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=ANHTU;'
+        'SERVER=HOANGVIET\SQLEXPRESS;'
         'DATABASE=QLSVNhom;'
         'Trusted_Connection=yes;'
     )
